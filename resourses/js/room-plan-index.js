7aafa88e228934e3dbf45fb32b034d969012796a
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-01-30 17:30:11 +0300

    Basic render. Resolves #1

--- resourses/js/room-plan-index.js
+++ resourses/js/room-plan-index.js
@@ -1,2 +1,8 @@
 // Run `npm run room-plan-build` to regenerate room-plan-bundle.js
 
+RoomPlanApp = require('./room-plan/App.js');
+
+$(function () {
+    var app = new RoomPlanApp();
+    app.init();
+});
