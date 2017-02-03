6de84a115a5b3c0cf6469a0ed88d283b94cb9851
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-02-04 00:08:46 +0300

    Door removal

--- resourses/js/room-plan/DoorBuilder.js
+++ resourses/js/room-plan/DoorBuilder.js
@@ -4,56 +4,107 @@ function DoorBuilder(wallsCollection) {
 module.exports = DoorBuilder;
 
 DoorBuilder.prototype.tryAddDoor = function (x, y) {
-    var done = false;
-    for (var i = 0; i < this.wallsCollection.walls.length; i++) {
-        var wall = this.wallsCollection.walls[i];
-        for (var j = 0; j < wall.cells.length; j++) {
-            var cell = wall.cells[j];
-            if (cell.x == x && cell.y == y) {
-                var hasDoor = this._hasDoor(cell);
-                if (!hasDoor) {
-                    var neighborhood = wall.getCellNeighborhood(cell);
-                    var cell0 = null;
-                    var cell1 = cell;
-                    var cell2 = null;
-                    if (neighborhood.isVerticalLine()) {
-                        cell0 = neighborhood.up;
-                        cell2 = neighborhood.down;
-                        var cell0Neighborhood = wall.getCellNeighborhood(cell0);
-                        var cell2Neighborhood = wall.getCellNeighborhood(cell2);
-                        if (cell0Neighborhood.left || cell0Neighborhood.right || cell2Neighborhood.left || cell2Neighborhood.right) {
-                            break;
-                        }
-                    } else if (neighborhood.isHorizontalLine()) {
-                        cell0 = neighborhood.left;
-                        cell2 = neighborhood.right;
-                        var cell0Neighborhood = wall.getCellNeighborhood(cell0);
-                        var cell2Neighborhood = wall.getCellNeighborhood(cell2);
-                        if (cell0Neighborhood.up || cell0Neighborhood.down || cell2Neighborhood.up || cell2Neighborhood.down) {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-                    if (!this._hasDoor(cell0) && !this._hasDoor(cell1) && !this._hasDoor(cell2)) {
-                        this._createDoor(cell0, cell1, cell2);
-                        done = true;
-                    }
-                }
-                break;
-            }
-        }
-        if (done) {
-            this.wallsCollection.wallViews[i][0].renderWall();
-            break;
-        }
+    var d = this.wallsCollection.findCellAndWall(x, y);
+    if (d.cell == null) {
+        return false;
     }
+    var hasDoor = this._hasDoor(d.cell);
+    if (hasDoor) {
+        return false;
+    }
+    var neighborhood = d.wall.getCellNeighborhood(d.cell);
+    var cell0 = null;
+    var cell1 = d.cell;
+    var cell2 = null;
+    if (neighborhood.isVerticalLine()) {
+        cell0 = neighborhood.up;
+        cell2 = neighborhood.down;
+        var cell0Neighborhood = d.wall.getCellNeighborhood(cell0);
+        var cell2Neighborhood = d.wall.getCellNeighborhood(cell2);
+        if (cell0Neighborhood.left || cell0Neighborhood.right || cell2Neighborhood.left || cell2Neighborhood.right) {
+            return false;
+        }
+    } else if (neighborhood.isHorizontalLine()) {
+        cell0 = neighborhood.left;
+        cell2 = neighborhood.right;
+        var cell0Neighborhood = d.wall.getCellNeighborhood(cell0);
+        var cell2Neighborhood = d.wall.getCellNeighborhood(cell2);
+        if (cell0Neighborhood.up || cell0Neighborhood.down || cell2Neighborhood.up || cell2Neighborhood.down) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+    if (this._hasDoor(cell0) || this._hasDoor(cell1) || this._hasDoor(cell2)) {
+        return false;
+    }
+    this._createDoor(cell0, cell1, cell2);
+    d.wallViews[0].renderWall();
+    return true;
+};
+
+DoorBuilder.prototype.tryRemoveDoor = function (x, y) {
+    var d = this.wallsCollection.findCellAndWall(x, y);
+    if (d.cell == null) {
+        return false;
+    }
+    var doorType = this._getDoorType(d.cell);
+    if (doorType == null) {
+        return false;
+    }
+    var neighborhood = d.wall.getCellNeighborhood(d.cell);
+    switch (doorType) {
+        case "door0":
+            d.cell.contents.remove("door0");
+            if (neighborhood.right) {
+                d.wall.getCell(d.cell.x + 1, d.cell.y).contents.remove("door1");
+                d.wall.getCell(d.cell.x + 2, d.cell.y).contents.remove("door2");
+            } else if (neighborhood.down) {
+                d.wall.getCell(d.cell.x, d.cell.y + 1).contents.remove("door1");
+                d.wall.getCell(d.cell.x, d.cell.y + 2).contents.remove("door2");
+            }
+            break;
+
+        case "door1":
+            d.cell.contents.remove("door1");
+            if (neighborhood.right) {
+                d.wall.getCell(d.cell.x - 1, d.cell.y).contents.remove("door0");
+                d.wall.getCell(d.cell.x + 1, d.cell.y).contents.remove("door2");
+            } else if (neighborhood.down) {
+                d.wall.getCell(d.cell.x, d.cell.y - 1).contents.remove("door0");
+                d.wall.getCell(d.cell.x, d.cell.y + 1).contents.remove("door2");
+            }
+            break;
+
+        case "door2":
+            d.cell.contents.remove("door2");
+            if (neighborhood.left) {
+                d.wall.getCell(d.cell.x - 2, d.cell.y).contents.remove("door0");
+                d.wall.getCell(d.cell.x - 1, d.cell.y).contents.remove("door1");
+            } else if (neighborhood.up) {
+                d.wall.getCell(d.cell.x, d.cell.y - 2).contents.remove("door0");
+                d.wall.getCell(d.cell.x, d.cell.y - 1).contents.remove("door1");
+            }
+            break;
+    }
+    d.wallViews[0].renderWall();
+    return true;
 };
 
 DoorBuilder.prototype._hasDoor = function (cell) {
     return _.some([0, 1, 2], function (i) {
         return cell.contents.has("door" + i);
     });
+};
+
+DoorBuilder.prototype._getDoorType = function (cell) {
+    var i = _.find([0, 1, 2], function (i) {
+        return cell.contents.has("door" + i);
+    });
+    if (i == null) {
+        return null;
+    }
+    return "door" + i;
 };
 
 DoorBuilder.prototype._createDoor = function () {
