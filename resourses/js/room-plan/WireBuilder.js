51c927de19addad27adec5c132fb2ed8927f7657
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-02-04 00:28:49 +0300

    Wire removal

--- resourses/js/room-plan/WireBuilder.js
+++ resourses/js/room-plan/WireBuilder.js
@@ -4,25 +4,33 @@ function WireBuilder(wallsCollection) {
 module.exports = WireBuilder;
 
 WireBuilder.prototype.tryAddWire = function (x, y) {
-    var done = false;
-    for (var i = 0; i < this.wallsCollection.walls.length; i++) {
-        var wall = this.wallsCollection.walls[i];
-        for (var j = 0; j < wall.cells.length; j++) {
-            var cell = wall.cells[j];
-            if (cell.x == x && cell.y == y) {
-                var hasConnection = _.some(wall.getCellNeighborhood(cell).toArray(), function (otherCell) {
-                    return otherCell.contents.has("wire");
-                });
-                if (hasConnection) {
-                    cell.contents.add("wire");
-                    done = true;
-                }
-                break;
-            }
-        }
-        if (done) {
-            this.wallsCollection.wallViews[i][0].renderWall();
-            break;
-        }
+    var d = this.wallsCollection.findCellAndWall(x, y);
+    if (d.cell == null) {
+        return false;
     }
+    var hasConnection = _.some(d.wall.getCellNeighborhood(d.cell).toArray(), function (otherCell) {
+        return otherCell.contents.has("wire");
+    });
+    if (!hasConnection) {
+        return false;
+    }
+    d.cell.contents.add("wire");
+    d.wallViews[0].renderWall();
+    return true;
+};
+
+WireBuilder.prototype.tryRemoveWire = function (x, y) {
+    var d = this.wallsCollection.findCellAndWall(x, y);
+    if (d.cell == null) {
+        return false;
+    }
+    var connections = _.filter(d.wall.getCellNeighborhood(d.cell).toArray(), function (otherCell) {
+        return otherCell.contents.has("wire");
+    });
+    if (connections.length > 1) {
+        return false;
+    }
+    d.cell.contents.delete("wire");
+    d.wallViews[0].renderWall();
+    return true;
 };
