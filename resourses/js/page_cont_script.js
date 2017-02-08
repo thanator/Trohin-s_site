a3e2027f439f4f12185d435a4ebb2a4adf4f4c00
Redhper <redhper@gmail.com>
2017-02-08 16:35:15 +0300

    ?

---
+++ resourses/js/page_cont_script.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+
+
+    $('.js__work_0_to_1').click(function () {
+         $('.exam_box_1').removeClass('disp');
+         $('.exam_box').addClass('disp');
+    });
+
+    $('.js__work_1_to_0').click(function () {
+         $('.exam_box').removeClass('disp');
+         $('.exam_box_1').addClass('disp');
+    });
+
+        $('.js__work_0_to_2').click(function () {
+         $('.exam_box_2').removeClass('disp');
+         $('.exam_box').addClass('disp');
+    });
+
+    $('.js__work_2_to_0').click(function () {
+         $('.exam_box').removeClass('disp');
+         $('.exam_box_2').addClass('disp');
+    });
+
+            $('.js__work_0_to_3').click(function () {
+         $('.exam_box_3').removeClass('disp');
+         $('.exam_box').addClass('disp');
+    });
+
+    $('.js__work_3_to_0').click(function () {
+         $('.exam_box').removeClass('disp');
+         $('.exam_box_3').addClass('disp');
+    });
+
+            $('.js__work_0_to_4').click(function () {
+         $('.exam_box_4').removeClass('disp');
+         $('.exam_box').addClass('disp');
+    });
+
+    $('.js__work_4_to_0').click(function () {
+         $('.exam_box').removeClass('disp');
+         $('.exam_box_4').addClass('disp');
+    });
+
+                $('.js__work_0_to_5').click(function () {
+         $('.exam_box_5').removeClass('disp');
+         $('.exam_box').addClass('disp');
+    });
+
+    $('.js__work_5_to_0').click(function () {
+         $('.exam_box').removeClass('disp');
+         $('.exam_box_5').addClass('disp');
+    });
+
+                    $('.js__work_0_to_6').click(function () {
+         $('.exam_box_6').removeClass('disp');
+         $('.exam_box').addClass('disp');
+    });
+
+    $('.js__work_6_to_0').click(function () {
+         $('.exam_box').removeClass('disp');
+         $('.exam_box_6').addClass('disp');
+    });
+
+});
+
