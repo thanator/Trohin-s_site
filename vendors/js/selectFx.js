f15587134dfcb767248434d281852f3e80828427
Redhper <redhper@gmail.com>
2017-02-10 18:33:36 +0300

    Prices selector and nav

--- vendors/js/selectFx.js
+++ vendors/js/selectFx.js
@@ -139,7 +139,7 @@
 		this.selEl = document.createElement( 'div' );
 		this.selEl.className = this.el.className;
 		this.selEl.tabIndex = this.el.tabIndex;
-		this.selEl.innerHTML = '<span class="cs-placeholder">' + this.selectedOpt.textContent + '</span>' + opts_el;
+		this.selEl.innerHTML = '<span class="cs-placeholder">' + 'Выбрать вид работы' + '</span>' + opts_el;
 		this.el.parentNode.appendChild( this.selEl );
 		this.selEl.appendChild( this.el );
 	}
