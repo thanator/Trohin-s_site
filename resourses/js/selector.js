37a01c0d06692261cc83e492bb9269df79ab97b7
Redhper <redhper@gmail.com>
2017-02-14 19:44:15 +0300

    selector

---
+++ resourses/js/selector.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+     $('.cs-options ul li:first-child').click(function() {
+        $('#label-1').css('display', 'block');
+        $('#label-2').css('display', 'none');
+        $('#label-3').css('display', 'none');
+        $('#label-4').css('display', 'none');
+        $('#label-5').css('display', 'none');
+     });
+});
